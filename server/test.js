12040429a8af7d09360b48fe516708cd4806c50d
William-Boomer-Baker <pasindu.mendis97@gmail.com>
2022-11-25 15:02:57 +0530

    shortId

--- server/test.js
+++ server/test.js
@@ -44,7 +44,7 @@ exports.handler = async (event, context) => {
 
   }
 }; */
-const User = require("./customFunctions/userModel");
+const shortid = require("shortid");
 
 exports.handler = async (event, context) => {
     //const existingUser = await User.findOne({ email: "email" });
