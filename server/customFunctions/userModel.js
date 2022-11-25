1f89d8d46776e372a756f479a6cb59b8441ca5bb
William-Boomer-Baker <pasindu.mendis97@gmail.com>
2022-11-25 14:52:26 +0530

    shortId

--- server/customFunctions/userModel.js
+++ server/customFunctions/userModel.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+var mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://Neural-Leap-Ventures:Neural-Leap-Ventures@neural-leap-ventures.3ugshp3.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true,
+  useUnifiedTopology: true, useFindAndModify: false});
+var Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     userId: String,
--- server/submit.js
+++ server/submit.js
@@ -1,39 +1,36 @@
-const mongoose = require('mongoose');
-//const User = require("./customFunctions/userModel");
-//const shortid = require("shortid");
-/* const cookie = require('cookie'); */
+const User = require("./customFunctions/userModel");
+const shortid = require("shortid");
+const cookie = require('cookie');
 
 exports.handler = async (event, context) => {
   console.log(event.body)
   const array = event.body.split("email=");
   const email = decodeURIComponent(array[1]);
-/*   const myCookie = cookie.serialize('emailHash', email); */
+  const myCookie = cookie.serialize('emailHash', email);
 
   try {
-    //mongoose.connect("mongodb+srv://Neural-Leap-Ventures:Neural-Leap-Ventures@neural-leap-ventures.3ugshp3.mongodb.net/?retryWrites=true&w=majority");
-    const existingUser = "await User.findOne({ email: email });"
+    const existingUser = await User.findOne({ email: email });
 
     if (existingUser) {
 
     }
 
     if (!existingUser) {
-      //const shortIdVariable = shortid.generate();
+      const shortIdVariable = shortid.generate();
 
-      /* const user = await new User({
+      const user = await new User({
         email: email,
-        referralId: "shortIdVariable",
+        referralId: shortIdVariable,
         numberOfReferrals: 0
-      }).save(); */
+      }).save();
     }
-    mongoose.disconnect();
 
     return {
       statusCode: 302,
-      /* headers: {
+      headers: {
         "Location": "https://breakpoints.ai/early-access",
         'Set-Cookie': myCookie
-      }, */
+      },
       body: "Success",
     };
 
@@ -43,6 +40,6 @@ exports.handler = async (event, context) => {
       statusCode: 400,
       body: err,
     };
+
   }
 };
-
