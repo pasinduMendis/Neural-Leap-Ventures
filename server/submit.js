fc9941d32d91bca40a2df4ef84dc9c3c9ff865f9
William-Boomer-Baker <pasindu.mendis97@gmail.com>
2022-11-25 12:04:25 +0530

    add project

---
+++ server/submit.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+require("./customFunctions/userModel");
+const User = mongoose.model("users");
+const shortid = require("shortid");
+const cookie = require('cookie');
+
+exports.handler = async (event, context) => {
+
+  const array = event.body.split("email=");
+  const email = decodeURIComponent(array[1]);
+  const myCookie = cookie.serialize('emailHash', email);
+
+  try {
+    mongoose.connect(process.env.MONGO_URI ||"mongodb+srv://Neural-Leap-Ventures:Neural-Leap-Ventures@neural-leap-ventures.3ugshp3.mongodb.net/?retryWrites=true&w=majority");
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+
+    }
+
+    if (!existingUser) {
+      const shortIdVariable = shortid.generate();
+
+      const user = await new User({
+        email: email,
+        referralId: shortIdVariable,
+        numberOfReferrals: 0
+      }).save();
+    }
+    mongoose.disconnect();
+
+    return {
+      statusCode: 302,
+      headers: {
+        "Location": "https://breakpoints.ai/early-access",
+        'Set-Cookie': myCookie
+      },
+      body: "Success",
+    };
+
+  } catch (err) {
+
+    return {
+      statusCode: 400,
+      body: err,
+    };
+
+  }
+};
