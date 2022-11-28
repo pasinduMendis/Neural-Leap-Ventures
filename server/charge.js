2ec60f515645c63105e5b319100e7f6c3e193816
William-Boomer-Baker <boomer@boomerbaker.com>
2022-11-28 10:20:54 +0530

    test7

--- server/charge.js
+++ server/charge.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 302,
       headers: {
-        "Location": "https://breakpoints.ai/thank-you-early-access",
+        "Location": "/thank-you-early-access",
         'Set-Cookie': myCookie
       },
       body: "Success",
